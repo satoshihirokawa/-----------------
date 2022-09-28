a4a2e5515cc23ffa393c6176b805106645a2394f
satoshihirokawa <smart.wide.river@gmail.com>
2022-09-28 18:14:03 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,57 @@
+{
+  /*=================================================
+  ハンバーガ―メニュー
+  ===================================================*/
+
+  // 変数宣言
+  // ハンバーガーメニューの要素
+  const hamburgerMenu = document.querySelector(".toggle_btn");
+
+  // ハンバーガーメニューがクリックされた時に表示されるメニュー画面の要素
+  const navi = document.getElementById("header");
+
+  // ハンバーガーメニュー内の各セクションの要素
+  const humbergerMenuSections = document.querySelectorAll(".nav-menu");
+
+  const mask = document.getElementById("mask");
+
+  // ハンバーガーメニューをクリックした時の処理
+  hamburgerMenu.addEventListener("click", function () {
+    // toggleを使用することで、wrapperクラスに
+    // openクラスが存在する場合は削除、存在しない場合を追加する処理を自動で行ってくれる
+    hamburgerMenu.classList.toggle("open");
+    navi.classList.toggle('open');
+    mask.classList.toggle('open');
+  });
+
+  // ハンバーガーメニュー内の各セクションをクリックした際、開いていたハンバーガーメニューを閉じる
+  humbergerMenuSections.forEach((humbergerMenuSection) => {
+    humbergerMenuSection.addEventListener("click", function () {
+      hamburgerMenu.classList.remove("open");
+      navi.classList.remove("open");
+      mask.classList.remove("open");
+    });
+  });
+
+  mask.addEventListener("click", function () {
+    hamburgerMenu.classList.remove("open");
+    navi.classList.remove("open");
+    mask.classList.remove("open");
+  });
+}
+
+//スライド by Swiper
+
+const swiper = new Swiper('.swiper', {
+centeredSlides:true,
+loop: true,
+spaceBetween: 60,
+slidesPerView:1.5,
+breakpoints: {
+  768: {
+    slidesPerView:3.9
+  },
+loopedSlides: 3,
+slideToClickedSlide: true,
+}
+});
